27deecd2bcc2409000c24663b105b83faed56df0
GreeceArtemis <greece.rapu@gmail.com>
2017-06-20 18:12:47 -0500

    hito1

--- public/assets/js/components/validatephone.js
+++ public/assets/js/components/validatephone.js
@@ -5,11 +5,11 @@ const Validatephone = (update) =>{
   const row              = $('<div class="row"></div>');
   const carouselSlider   = $('<div class=" center col m12" data-indicators="true"></div>');
   const carouselItem1    = $('<div class="" href="#one!"></div>');
-  const imgCarousel1     = $('<div class="img-validate-phone"></div>');
-  const imgItem1         = $('<img src="assets/img/icons/icon-people.png" alt="icon-people">');
-  const titleItem1       = $('<h2>Para comenzar validemos tu numero</h2>');
+  const imgCarousel1     = $('<div class="img-validate-phone col s8 m9"></div>');
+  const imgItem1         = $('<img src="assets/img/icons/phone.png" alt="icon-phone">');
+  const titleItem1       = $('<h3>Para comenzar validemos tu numero</h3>');
   const descriptionItem1 = $('<p class="">recibiras un SMS <br> quieras,sin usar efectivo</p>');
-  const btnContinuar     = $('<a class="btn col s10 offset-s1 background-yellow waves-effect center white-text darken-text-2">Registrarme</a>');
+  const btnContinuar     = $('<a class="btn col s10 offset-s1 background-yellow waves-effect center white-text darken-text-2">Continuar</a>');
 
   const input        = $('<input  placeholder="numero">');
 
